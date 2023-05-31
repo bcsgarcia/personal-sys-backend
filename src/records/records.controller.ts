ad3c0cbe17018d396483d5fbbfce80cf10c8b36c
Bruno Garcia <bcsgarcia@outlook.com>
2023-05-31 21:13:06 +0100

    .

--- src/records/records.controller.ts
+++ src/records/records.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { Public } from 'src/api/auth/jwt.decorator';
 import { DatabaseService } from 'src/database/database.service';
 
+@Public()
 @Controller('records')
 export class RecordsController {
   constructor(private readonly databaseService: DatabaseService) {}
