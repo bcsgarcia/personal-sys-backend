ad85554e786758c9e3ab84619e64fbf4a0bf1762
Bruno Garcia <bcsgarcia@outlook.com>
2023-07-10 23:58:57 +0100

    .

--- src/api/upload/service/upload.service.ts
+++ src/api/upload/service/upload.service.ts
@@ -24,6 +24,7 @@ export class UploadService {
         const thumbnailBuffer = await this.thumbnailService.createThumbnail(
           file.buffer,
           file.mimetype,
+          idMedia,
         );
 
         // save thumbnail to ftp
