b5d2754bd7c5cedda4e37e55254a74298ae7d3bc
Bruno Garcia <bcsgarcia@outlook.com>
2024-02-06 22:04:16 +0000

    ...

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(port, () => {
+  await app.listen(port, '0.0.0.0', () => {
     console.log(`App listening on port ${process.env.PORT}`);
   });
 }
