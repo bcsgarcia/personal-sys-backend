99940c41834191bffb7c1e92913ddb7c92ca4c87
Bruno Garcia <bcsgarcia@outlook.com>
2025-03-02 00:47:53 +0000

    .

--- src/middlewares/url-rewrite.middleware.ts
+++ src/middlewares/url-rewrite.middleware.ts
@@ -4,9 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class UrlRewriteMiddleware implements NestMiddleware {
   use(req: Request, _res: Response, next: NextFunction) {
-    // Remove /personal do início da URL se existir
-    if (req.url.includes('/personal')) {
+    // Remove /personal do início do path se existir
+    if (req.path.startsWith('/personal')) {
       req.url = req.url.replace('/personal', '');
+      console.log(`/personal removed from url:${req.url} `);
     }
     next();
   }
